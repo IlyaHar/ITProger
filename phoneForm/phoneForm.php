4cfa48c96e67167ac7999767d7484a2d9d3bc530
IlyaHar <harcenkoila278@gmail.com>
2023-12-27 16:00:56 +0200

    cookie forms

--- phoneForm/phoneForm.php
+++ phoneForm/phoneForm.php
@@ -12,7 +12,7 @@ if (!empty($_COOKIE['phone'])) {
 <div class="container">
     <h3 class="text-secondary">Номер телефона</h3>
     <form action="" method="POST">
-        <input type="text" class="form-control mt-3" name="name" placeholder="Введите свое имя..">
+        <input type="text" class="form-control mt-4" name="name" placeholder="Введите свое имя..">
         <input type="text" class="form-control mt-3" name="surname" placeholder="Введите свою фамилию..">
         <input type="tel" class="form-control mt-3" name="phone" value="<?= $phoneNumber ?>" placeholder="Введите свой номер телефона..">
         <input type="submit" class="btn btn-primary mt-3">
